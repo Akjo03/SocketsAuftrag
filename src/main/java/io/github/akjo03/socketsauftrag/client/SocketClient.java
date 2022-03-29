7c4270d7ae3ccd44f7752e27fb0d270420ca16ff
Justin Ceronio <justin.ceronio@outlook.com>
2022-03-29 12:57:57 +0200

    Implement Client

--- src/main/java/io/github/akjo03/socketsauftrag/client/SocketClient.java
+++ src/main/java/io/github/akjo03/socketsauftrag/client/SocketClient.java
@@ -1,14 +1,34 @@
 package io.github.akjo03.socketsauftrag.client;
 
-public class SocketClient implements Runnable {
-	private SocketClient() {}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
-	public static void main(String[] args) {
-		new SocketClient().run();
+public class SocketClient {
+
+	private Socket clientSocket;
+	private PrintWriter out;
+	private BufferedReader in;
+
+	public static void main(String[] args) { }
+
+	public void startConnection(String ip, int port) throws IOException {
+		clientSocket = new Socket(ip, port);
+		out = new PrintWriter(clientSocket.getOutputStream(), true);
+		in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 	}
 
-	@Override
-	public void run() {
+	public String sendMessage(String msg) throws IOException {
+		out.println(msg);
+		String resp = in.readLine();
+		return resp;
+	}
 
+	public void stopConnection() throws IOException {
+		in.close();
+		out.close();
+		clientSocket.close();
 	}
 }
