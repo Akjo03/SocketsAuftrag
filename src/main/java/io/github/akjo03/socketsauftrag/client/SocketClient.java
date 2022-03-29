a77142cbc9d1df628f68a5b209e1a828f161b6bc
AkjoHD <lukas.freckmann@gmx.ch>
2022-03-29 12:38:01 +0200

    Created classes for client and server

--- src/main/java/io/github/akjo03/socketsauftrag/client/SocketClient.java
+++ src/main/java/io/github/akjo03/socketsauftrag/client/SocketClient.java
@@ -1,11 +1,6 @@
 package io.github.akjo03.socketsauftrag.client;
 
-import io.github.akjo03.util.logging.v2.Logger;
-import io.github.akjo03.util.logging.v2.LoggerManager;
-
 public class SocketClient implements Runnable {
-	private static final Logger LOGGER = LoggerManager.getLogger(SocketClient.class);
-
 	private SocketClient() {}
 
 	public static void main(String[] args) {
