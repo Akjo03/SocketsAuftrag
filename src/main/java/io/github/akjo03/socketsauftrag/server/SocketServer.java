68cb8d8a6684ce167b1af278c175ee0a5835510c
RoadJDK <realroadjdk@gmail.com>
2022-03-29 12:56:16 +0200

    add server

--- src/main/java/io/github/akjo03/socketsauftrag/server/SocketServer.java
+++ src/main/java/io/github/akjo03/socketsauftrag/server/SocketServer.java
@@ -1,14 +1,36 @@
 package io.github.akjo03.socketsauftrag.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class SocketServer implements Runnable {
 	private SocketServer() {}
 
 	public static void main(String[] args) {
-		new SocketServer().run();
+		Thread ServerStart = new Thread(new SocketServer());
+		ServerStart.run();
 	}
 
 	@Override
 	public void run() {
 
+		try {
+			Scanner sc;
+			ServerSocket server = new ServerSocket(8090 );
+			System.out.println("(Server) Server running at: " + server.getLocalPort());
+
+			while (true) {
+				Socket socket = server.accept();
+				sc = new Scanner(socket.getInputStream());
+
+				System.out.println(sc.toString());
+			}
+
+
+		} catch (Exception e) {
+
+		}
 	}
 }
